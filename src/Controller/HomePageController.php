e606bcd3aa7c4021770be70e1eaf462797a809d9
Ayoub <ayoubaouinti1990@gmail.com>
2023-06-12 08:56:48 +0000

    second commit

--- src/Controller/HomePageController.php
+++ src/Controller/HomePageController.php
@@ -11,8 +11,6 @@ class HomePageController extends AbstractController
     #[Route('/', name: 'app_home_page')]
     public function index(): Response
     {
-        return $this->render('home_page/index.html.twig', [
-            'controller_name' => 'HomePageController',
-        ]);
+        return $this->render('home_page/index.html.twig');
     }
 }
